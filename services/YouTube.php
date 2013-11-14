9a2dd3382a9ceb3da64e8a050171058b8f5abc13
Danil Smirnov <aulust@gmail.com>
2013-11-15 00:25:00 +0400

    Fix youtube

--- services/YouTube.php
+++ services/YouTube.php
@@ -2,25 +2,25 @@
 require_once dirname(__FILE__).'/../StreamService.php';
 
 class YouTube extends StreamService {
-    
+
 	protected function loadChunkInfo($channels) {
 		return null;
 	}
-	
+
 	protected function decodeChunkInfo($raw) {
 		return null;
 	}
-	
+
 	public function checkChannel($channel) {
 		return true;
 	}
-	
+
     public function getThumbnail($channel) {
         return null;
     }
 
     public function getEmbedPlayerCode($channel, $width, $height) {
-        return $this->renderTemplate('player/youtube', array('channelId' => $channel['id'], 'width' => $width, 'height' => $height));
+        return $this->renderTemplate('player/youtube', array('channelId' => $channel['name'], 'width' => $width, 'height' => $height));
     }
 
 }
