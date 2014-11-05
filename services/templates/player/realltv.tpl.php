c4d818060791b079d40fe6a39e282db332fa7052
KingCobra <kingcobra@yandex.ru>
2014-11-06 00:28:23 +0300

    change ReallTv template

--- services/templates/player/realltv.tpl.php
+++ services/templates/player/realltv.tpl.php
@@ -1,1 +1,1 @@
-<iframe src="http://reall.tv/player/embed/<?= $width ?>/<?= $height ?>/<?= $channelName ?>" width="<?= $width ?>" height="<?= $height ?>" frameborder="0"></iframe>
+<iframe src="http://reall.tv/player/embed3/100/100/<?= $channelName ?>" width="<?= $width ?>" height="<?= $height ?>" frameborder="0" scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
