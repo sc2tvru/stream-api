7c585c37ef366e733408c10b30b7e6b9259d3744
KingCobra <kingcobra@yandex.ru>
2014-12-03 14:50:37 +0300

    add youtube support

--- services/YouTube.php
+++ services/YouTube.php
@@ -2,22 +2,47 @@
 require_once dirname(__FILE__).'/../StreamService.php';
 
 class YouTube extends StreamService {
-    const THUMBNAIL_IMAGE_URL = 'http://img.youtube.com/vi/:channel_id/default.jpg';
+
+	protected $chunk_size = 50;
+	
+    const DEV_KEY = 'AIzaSyDjvLZLAlFTB8WPETznMV3j6aIzoTM21F4';
+    const CHECK_STREAM_STATUS_URL = 'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails,snippet&id=:channels&key=:dev_key';
 
 	protected function loadChunkInfo($channels) {
-		return null;
+		$raw = $this->loadResource(strtr(self::CHECK_STREAM_STATUS_URL, array(':channels' => join(',',$channels), ':dev_key' => self::DEV_KEY)), array('maxResults'=>50), 'GET');
+		return $raw;
 	}
 
+	protected function mapInfo($v) {
+		
+		$channel_name = $v['id'];
+		$info = array(
+			'name' => $v['id'] ,
+			'id' => $v['id'],
+			'service' => StreamApiService::SERVICE_YOUTUBE,
+			'live' => $v['snippet']['liveBroadcastContent'] == 'live' ? true : false,
+			'thumbnail' => $v['snippet']['thumbnails']['medium']['url'],
+			'title' => $v['snippet']['title'],
+			'description' => $v['snippet']['description'],
+			'viewers' => (isset($v['liveStreamingDetails']['concurrentViewers']) ? (int)$v['liveStreamingDetails']['concurrentViewers'] : 0),
+		);
+		return $info;
+	}
+	
 	protected function decodeChunkInfo($raw) {
+		$res = json_decode($raw, true);
+		$info = array();
+		if($res) {
+			foreach($res['items'] as $v) {
+				$info[] = $this->mapInfo($v);
+			}
+			return $info;
+		}
 		return null;
-	}
-
-	public function checkChannel($channel) {
-		return true;
 	}
 
     public function getThumbnail($channel) {
-        return strtr(self::THUMBNAIL_IMAGE_URL, array(':channel_id' => $channel['name']));
+        return null;
     }
 
     public function getEmbedPlayerCode($channel, $width, $height) {
